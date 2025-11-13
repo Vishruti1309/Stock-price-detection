3a56afac47cd89b6065b1d96083301247beb0b82
Vishruti Mistry <vishruti1309@gmail.com>
2025-11-13 23:31:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
@@ -89,4 +89,5 @@ result_df = pd.DataFrame({
 })
 
 st.line_chart(result_df)
+
 
